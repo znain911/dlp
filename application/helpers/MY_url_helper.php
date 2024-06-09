671a87204208b4f4df58ed97fc81bd0610011c3e
Zulkar Nain <znain911@gmail.com>
2024-06-09 12:39:22 +0600

    Update MY_url_helper.php
    removing confidential info

--- application/helpers/MY_url_helper.php
+++ application/helpers/MY_url_helper.php
@@ -28,12 +28,12 @@ function current_url()
 }*/
 
 function sendsms($number, $message) {
-  $url = "http://sms.iccteleservices.com/smsapi";
+  $url = "";
   $data = [
-    "api_key" => "C20070275f66f94e214d64.69540581",
+    "api_key" => "",
     "type" => "text",
     "contacts" => $number,
-    "senderid" => "8809601000127",
+    "senderid" => "",
     "msg" => json_encode($message),
   ];
   $ch = curl_init();
